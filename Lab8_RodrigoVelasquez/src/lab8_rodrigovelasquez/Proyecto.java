c04d69796c082736904ead93e6ca0858a20d91aa
rodrix183 <rodrix183@hotmail.com>
2018-09-14 13:21:06 -0600

    Atributos Proyecto

--- Lab8_RodrigoVelasquez/src/lab8_rodrigovelasquez/Proyecto.java
+++ Lab8_RodrigoVelasquez/src/lab8_rodrigovelasquez/Proyecto.java
@@ -1,5 +1,48 @@
 package lab8_rodrigovelasquez;
 
+import java.util.ArrayList;
+
 public class Proyecto {
+    private String nombre;
+    private int duracion;
+    private ArrayList<Actividades>lista_actividades = new ArrayList<>();
+
+    public Proyecto() {
+    }
+
+    public Proyecto(String nombre, int duracion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public ArrayList<Actividades> getLista_actividades() {
+        return lista_actividades;
+    }
+
+    public void setLista_actividades(ArrayList<Actividades> lista_actividades) {
+        this.lista_actividades = lista_actividades;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
     
 }
