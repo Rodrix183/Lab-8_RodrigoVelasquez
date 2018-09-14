6c0d4ec9ccfe3726b2f731468335eb0a4b9c7596
rodrix183 <rodrix183@hotmail.com>
2018-09-14 13:31:33 -0600

    Clase actividades atributos

--- Lab8_RodrigoVelasquez/src/lab8_rodrigovelasquez/Actividades.java
+++ Lab8_RodrigoVelasquez/src/lab8_rodrigovelasquez/Actividades.java
@@ -1,5 +1,87 @@
 package lab8_rodrigovelasquez;
 
+import java.util.ArrayList;
+
 public class Actividades {
+    private String nombre;
+    private int inicio_temp;
+    private int duracion;
+    private int final_temp;
+    private int p_retraso;
+    private ArrayList<Actividades> sucesores = new ArrayList<>();
+    private ArrayList<Actividades> predecesores = new ArrayList<>();
+
+    public Actividades() {
+    }
+
+    public Actividades(String nombre, int inicio_temp, int duracion, int final_temp, int p_retraso) {
+        this.nombre = nombre;
+        this.inicio_temp = inicio_temp;
+        this.duracion = duracion;
+        this.final_temp = final_temp;
+        this.p_retraso = p_retraso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getInicio_temp() {
+        return inicio_temp;
+    }
+
+    public void setInicio_temp(int inicio_temp) {
+        this.inicio_temp = inicio_temp;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public int getFinal_temp() {
+        return final_temp;
+    }
+
+    public void setFinal_temp(int final_temp) {
+        this.final_temp = final_temp;
+    }
+
+    public int getP_retraso() {
+        return p_retraso;
+    }
+
+    public void setP_retraso(int p_retraso) {
+        this.p_retraso = p_retraso;
+    }
+
+    public ArrayList<Actividades> getSucesores() {
+        return sucesores;
+    }
+
+    public void setSucesores(ArrayList<Actividades> sucesores) {
+        this.sucesores = sucesores;
+    }
+
+    public ArrayList<Actividades> getPredecesores() {
+        return predecesores;
+    }
+
+    public void setPredecesores(ArrayList<Actividades> predecesores) {
+        this.predecesores = predecesores;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
     
 }
